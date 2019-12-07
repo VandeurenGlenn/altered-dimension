178e6c6654ca19ac9f514f921cdf1f668f646494
vandeurenglenn <vandeurenglenn@gmail.com>
2019-12-07 23:34:21 +0100

    Deployed to Github pages: 5c5f5bc50c339597c7eab409cc8bf7e40a3fb180

--- app.js
+++ app.js
@@ -206,6 +206,257 @@ define(class CustomSelector extends SelectorMixin(HTMLElement) {
   }
 });
 
+//
+/**
+* Replaces all accented chars with regular ones
+*/
+const replaceAccents = string => {
+  // verifies if the String has accents and replace them
+  if (string.search(/[\xC0-\xFF]/g) > -1) {
+      string = string
+              .replace(/[\xC0-\xC5]/g, "A")
+              .replace(/[\xC6]/g, "AE")
+              .replace(/[\xC7]/g, "C")
+              .replace(/[\xC8-\xCB]/g, "E")
+              .replace(/[\xCC-\xCF]/g, "I")
+              .replace(/[\xD0]/g, "D")
+              .replace(/[\xD1]/g, "N")
+              .replace(/[\xD2-\xD6\xD8]/g, "O")
+              .replace(/[\xD9-\xDC]/g, "U")
+              .replace(/[\xDD]/g, "Y")
+              .replace(/[\xDE]/g, "P")
+              .replace(/[\xE0-\xE5]/g, "a")
+              .replace(/[\xE6]/g, "ae")
+              .replace(/[\xE7]/g, "c")
+              .replace(/[\xE8-\xEB]/g, "e")
+              .replace(/[\xEC-\xEF]/g, "i")
+              .replace(/[\xF1]/g, "n")
+              .replace(/[\xF2-\xF6\xF8]/g, "o")
+              .replace(/[\xF9-\xFC]/g, "u")
+              .replace(/[\xFE]/g, "p")
+              .replace(/[\xFD\xFF]/g, "y");
+  }
+
+  return string;
+};
+
+const removeNonWord = string => string.replace(/[^0-9a-zA-Z\xC0-\xFF \-]/g, '');
+
+const WHITE_SPACES = [
+    ' ', '\n', '\r', '\t', '\f', '\v', '\u00A0', '\u1680', '\u180E',
+    '\u2000', '\u2001', '\u2002', '\u2003', '\u2004', '\u2005', '\u2006',
+    '\u2007', '\u2008', '\u2009', '\u200A', '\u2028', '\u2029', '\u202F',
+    '\u205F', '\u3000'
+];
+
+/**
+* Remove chars from beginning of string.
+*/
+const ltrim = (string, chars) => {
+  chars = chars || WHITE_SPACES;
+
+  var start = 0,
+      len = string.length,
+      charLen = chars.length,
+      found = true,
+      i, c;
+
+  while (found && start < len) {
+      found = false;
+      i = -1;
+      c = string.charAt(start);
+
+      while (++i < charLen) {
+          if (c === chars[i]) {
+              found = true;
+              start++;
+              break;
+          }
+      }
+  }
+
+  return (start >= len) ? '' : string.substr(start, len);
+};
+
+/**
+* Remove chars from end of string.
+*/
+const rtrim = (string, chars) => {
+  chars = chars || WHITE_SPACES;
+
+  var end = string.length - 1,
+      charLen = chars.length,
+      found = true,
+      i, c;
+
+  while (found && end >= 0) {
+      found = false;
+      i = -1;
+      c = string.charAt(end);
+
+      while (++i < charLen) {
+          if (c === chars[i]) {
+              found = true;
+              end--;
+              break;
+          }
+      }
+  }
+
+  return (end >= 0) ? string.substring(0, end + 1) : '';
+};
+
+/**
+ * Add space between camelCase text.
+ */
+const unCamelCase = string => {
+  string = string.replace(/([a-z\xE0-\xFF])([A-Z\xC0\xDF])/g, '$1 $2');
+  string = string.toLowerCase();
+  return string;
+};
+
+/**
+ * Remove white-spaces from beginning and end of string.
+ */
+const trim = (string, chars) => {
+  chars = chars || WHITE_SPACES;
+  return ltrim(rtrim(string, chars), chars);
+};
+
+/**
+ * Convert to lower case, remove accents, remove non-word chars and
+ * replace spaces with the specified delimeter.
+ * Does not split camelCase text.
+ */
+const slugify = (string, delimeter) => {
+  if (delimeter == null) {
+      delimeter = "-";
+  }
+
+  string = replaceAccents(string);
+  string = removeNonWord(string);
+  string = trim(string) //should come after removeNonWord
+          .replace(/ +/g, delimeter) //replace spaces with delimeter
+          .toLowerCase();
+  return string;
+};
+
+/**
+* Replaces spaces with hyphens, split camelCase text, remove non-word chars, remove accents and convert to lower case.
+*/
+const hyphenate = string => {
+  string = unCamelCase(string);
+  return slugify(string, "-");
+};
+
+
+ let sheduled = false;
+ const afterRenderQue = [];
+ const beforeRenderQue = [];
+
+ const callMethod = array => {
+   const context = array[0];
+   const callback = array[1];
+   const args = array[2];
+   try {
+     callback.apply(context, args);
+   } catch(e) {
+     setTimeout(() => {
+       throw e;
+     });
+   }
+ };
+
+ const flushQue = que => {
+   while (que.length) {
+     callMethod(que.shift);
+   }
+ };
+
+ const runQue = que => {
+   for (let i=0, l=que.length; i < l; i++) {
+     callMethod(que.shift());
+   }
+   sheduled = false;
+ };
+
+ const shedule = () => {
+   sheduled = true;
+   requestAnimationFrame(() => {
+     flushQue(beforeRenderQue);
+     setTimeout(() => {
+       runQue(afterRenderQue);
+     });
+   });
+ };
+
+ const RenderStatus = (() => {
+   window.RenderStatus = window.RenderStatus || {
+     afterRender: (context, callback, args) => {
+       if (!sheduled) {
+         shedule();
+       }
+       afterRenderQue.push([context, callback, args]);
+     },
+     beforeRender: (context, callback, args) => {
+       if (!sheduled) {
+         shedule();
+       }
+       beforeRenderQue.push([context, callback, args]);
+     }
+   };
+ })();
+
+ const shouldRegister = name => {
+   return customElements.get(name) ? false : true;
+ };
+
+ const define$1 = klass => {
+   const name = hyphenate(klass.name);
+   return shouldRegister(name) ? customElements.define(name, klass) : '';
+ };
+
+define$1(class CustomDivider extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+  }
+  connectedCallback() {
+    this.shadowRoot.innerHTML = `
+    <style>
+      :host {
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: flex;
+        width: 100%;
+        height: 1px;
+        border-bottom: 1px solid rgba(0, 0, 0, 0.12);
+        margin-top: -1px;
+      }
+      :host([top]) {
+        border-bottom: none;
+        margin-bottom: -1px;
+        border-top: 1px solid rgba(0, 0, 0, 0.12);
+      }
+      :host([left]) {
+        height: 100%;
+        width: 1px;
+        border-bottom: none;
+        margin-right: -1px;
+        border-left: 1px solid rgba(0, 0, 0, 0.12);
+      }
+      :host([right]) {
+        height: 100%;
+        width: 1px;
+        border-bottom: none;
+        left: -1px;
+        border-right: 1px solid rgba(0, 0, 0, 0.12);
+      }
+    </style>
+    `;
+  }
+});
+
 var app = customElements.define('altered-app', class AlteredApp extends HTMLElement {
   get drawerToggle() {
     return this.querySelector('.drawer-menu-icon')  
@@ -258,7 +509,7 @@ var app = customElements.define('altered-app', class AlteredApp extends HTMLElem
     (async () => {
       await import('./custom-drawer-b43c6f3e.js');
       // await customElements.whenDefined('custom-drawer')
-      await import('./custom-page-controller-1c0da1aa.js');
+      await import('./custom-page-controller-4a2d44bb.js');
       
       console.log(this.shadowRoot);
       window.go = this.go;
@@ -269,8 +520,15 @@ var app = customElements.define('altered-app', class AlteredApp extends HTMLElem
   }
   
   async go(route, subRoute, info) {
+    if (route === 'home') return
     const has = await customElements.get(`${route}-section`);
-    if (!has)
+    try {
+      if (!has) await import(`./${route}.js`);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      
+    }
     console.log(has);
   }
   
---
+++ custom-page-controller-4a2d44bb.js
@@ -0,0 +1,196 @@
+import { a as SelectMixin, S as SelectorMixin } from './selector-mixin-a6a7b665.js';
+
+/**
+ * @extends HTMLElement
+ */
+class CustomPages extends SelectMixin(HTMLElement) {
+  constructor() {
+    super();
+    this.slotchange = this.slotchange.bind(this);
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = `
+      <style>
+        :host {
+          flex: 1;
+          position: relative;
+          --primary-background-color: #ECEFF1;
+          overflow: hidden;
+        }
+        ::slotted(*) {
+          display: flex;
+          position: absolute;
+          opacity: 0;
+          pointer-events: none;
+          top: 0;
+          left: 0;
+          right: 0;
+          bottom: 0;
+          transition: transform ease-out 160ms, opacity ease-out 60ms;
+          /*transform: scale(0.5);*/
+          transform-origin: left;
+        }
+        ::slotted(.animate-up) {
+          transform: translateY(-120%);
+        }
+        ::slotted(.animate-down) {
+          transform: translateY(120%);
+        }
+        ::slotted(.custom-selected) {
+          opacity: 1;
+          pointer-events: auto;
+          transform: translateY(0);
+          transition: transform ease-in 160ms, opacity ease-in 320ms;
+          max-height: 100%;
+          max-width: 100%;
+        }
+      </style>
+      <!-- TODO: scale animation, ace doesn't resize that well ... -->
+      <div class="wrapper">
+        <slot></slot>
+      </div>
+    `;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.shadowRoot.querySelector('slot').addEventListener('slotchange', this.slotchange);
+  }
+
+  isEvenNumber(number) {
+    return Boolean(number % 2 === 0)
+  }
+
+  /**
+   * set animation class when slot changes
+   */
+  slotchange() {
+    let call = 0;
+    for (const child of this.slotted.assignedNodes()) {
+      if (child && child.nodeType === 1) {
+        child.style.zIndex = 99 - call;
+        if (this.isEvenNumber(call++)) {
+          child.classList.add('animate-down');
+        } else {
+          child.classList.add('animate-up');
+        }
+        this.dispatchEvent(new CustomEvent('child-change', {detail: child}));
+      }
+    }
+  }
+}customElements.define('custom-pages', CustomPages);
+
+customElements.define('custom-menu', class CustomMenu extends SelectorMixin(HTMLElement) {
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.shadowRoot.innerHTML = this.template;
+  }
+  
+  _onClick(event) {
+    super._onClick(event);
+    const sub = this.querySelector(`[custom-submenu][data-route="${this.selected}"]`);
+    if (sub && sub.dataset.route === this.selected) this._toggleSub(sub);
+  }
+  
+  _toggleSub(sub) {
+    const els = this.querySelectorAll(`[sub-menu-item="${this.selected}"]`);
+    if (sub.hasAttribute('opened')) sub.removeAttribute('opened');
+    else sub.setAttribute('opened', '');
+    for (const el of els) {
+      if (sub.hasAttribute('opened')) el.setAttribute('shown', '');
+      else {
+        if (el.hasAttribute('custom-submenu')) {
+          const elsInEl = this.querySelectorAll(`[sub-menu-item="${el.dataset.route}"]`);
+          el.removeAttribute('opened');
+          for (const el of elsInEl) {
+            el.removeAttribute('shown');
+          }
+        }
+        
+        el.removeAttribute('shown');
+      }
+    }
+    
+  }
+  
+  get template() {
+    return `<style>
+      :host {
+        display: flex;
+        flex-direction: column;
+      }
+      ::slotted([sub-menu-item]) {
+        transform: scale(0);
+        height: 0px !important;
+        padding: 0 !important;
+      }
+      ::slotted([sub-menu-item][shown]) {
+        height: auto !important;
+        padding: 12px 12px 12px 36px !important;
+        transform: scale(1);
+      }
+      ::slotted([sub-menu-item][in-custom-submenu][shown]) {
+        padding: 12px 12px 12px 64px !important;
+      }
+    </style>
+    <slot></slot>
+    `
+  }
+});
+
+customElements.define('custom-page-title', class CustomPageTitle extends HTMLElement {
+    get value() {
+      return this.getAttribute('value')
+    }
+    set value(value) {
+      this.setAttribute('value', value);
+      this.innerHTML = `<h3>${value}</h3>`;
+    }
+  constructor() {
+    super();
+    this.style.paddingLeft = '4px';
+    this.style.textTransform = 'uppercase';
+    this.innerHTML = `<h3>${this.value}</h3>`;
+  }
+  get template() {
+    return 
+  }
+});
+
+var customPageController = customElements.define('custom-page-controller', class CustomPageController extends HTMLElement {
+  set title(value) {
+    this._title.value = value;
+  }
+  set page(value) {
+    this._pages.select(value);
+  }
+  get _title() { return document.querySelector('custom-page-title') }
+  get _pages() { return document.querySelector('custom-pages') }
+  get _nav() { return document.querySelector('custom-menu') }
+  // set
+  constructor() {
+    super();
+    this.style.display = 'none';
+  }
+  
+  connectedCallback() {
+    this.whenDefined(['custom-pages', 'custom-page-title']);
+  }
+  
+  async whenDefined(elements) {
+    for (const element of elements) {
+      if (element === 'custom-pages') await customElements.whenDefined(element);
+      if (element === 'custom-page-title') this._setupListener();
+    }    
+  }
+  
+  _setupListener() {
+    this._nav.addEventListener('selected', ({detail}) => {
+      this.page = detail;
+      this.title = detail;
+      if (window.go) window.go(detail);
+    });
+  }
+});
+
+export default customPageController;
