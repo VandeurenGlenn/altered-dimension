3da511ca0d1384d6ab5bd1adae9c8e8e72d0cda6
TheAlteredDimension <59619892+TheAlteredDimension@users.noreply.github.com>
2020-01-07 19:39:40 +0100

    Update games.js

--- src/data/games.js
+++ src/data/games.js
@@ -27,7 +27,132 @@ export default {
       description: `
       Ga voor de High score van deze geweldige slomotion shooter.  Ontwijk, vang, gooi of schiet je weg door de kogels, scherven en brokstukken naar buiten.`,
       iframe: 'https://www.youtube-nocookie.com/embed/pzG7Wc6mbwE?controls=0'
-    }
+    }, {
+      title: 'Nature Trek',
+      subtitle: 'Relax and explore. Singleplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Dompel je zelf onder in een wereld van rust, een plaats waar je kan ademene en ontspannen. Ervaar vrijheid op een plaats speciaal voor jouw.`,
+      iframe: 'https://www.youtube.com/watch?v=h-LItx2xORA'
+    }, {
+      title: 'Beat Saber',
+      subtitle: 'Pumped up. Singleplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Het bekendste spel in Virual reality. Beat Saber is een VR ritme spel waar je al slagend door de beat van de adrenaline pompende muziek gaat. Try to keep it up.`,
+      iframe: 'https://www.youtube.com/watch?v=vL39Sg2AqWg'
+    }, {
+      title: 'Fruit ninja',
+      subtitle: 'Feel like a Ninja. Singleplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Het meest bevredigende en toegankelijke partygame ooit gemaakt komt naar virtual reality! Het fenomeen Fruit Ninja gaat weer door in de volgende generatie.`,
+      iframe: 'https://www.youtube.com/watch?v=H1CRhg2mDTI'
+    }, {
+      title: 'Space Pirate Trainer',
+      subtitle: 'I am a space pirate. Singleplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Space Pirate Trainer is het officiele trainings spel voor space pirates in VR. Pak je balster doe je schoene aan en dans je zelf een weg in de Space Pirate Trainer hall of fame.`,
+      iframe: 'https://www.youtube.com/watch?v=xfa9Jqm34b0'
+    }, {
+      title: 'Summer Funland',
+      subtitle: 'Fun and games. Singleplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Maak je klaar voor de geweldige VR-ervaring die Summer Funland is! Spectaculaire attracties en plezier in overvloed. Een gekke achtbaan, hilarische minispellen, een ongelooflijk snelle motorrit door Gotham, onderwaterreizen en ruimtetrip - Summer Funland zal je wegblazen!`,
+      iframe: 'https://www.youtube.com/watch?v=6taGudtLLOo'
+    }, {
+      title: 'Creed Rise to Glorie',
+      subtitle: 'Boksen en skills. Co-op',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: true,
+      description: `
+      Heb jij het in huis om kampioen te worden? Volg de beklimming van bokser Adonis Creed terwijl je het opneemt tegen de beste tegenstanders van de wereld in deze intense, filmische bokservaring. Vecht, train en win als een Creed.`,
+      iframe: 'https://www.youtube.com/watch?v=A0lMg-loz0E'
+    }, {
+      title: 'Sweet Escape',
+      subtitle: 'Fun and Candy. PVP',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: true,
+      description: `
+      Ben jij de eerste? Ga de strijd aan, klim en race tegen je vrienden op en neer snoeplandschappen in deze wilde Virtual Reality-ervaring.`,
+      iframe: 'https://www.youtube.com/watch?v=tJHZrgucBgk'
+    }, {
+      title: 'Smashbox',
+      subtitle: 'Funny Shooter multiplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: true,
+      description: `
+      Teamgebaseerd actiespel met krankzinnige fysica-aangedreven power-ups. Schiet, duik achter dekking en coördineer met teamgenoten ...`,
+      iframe: 'https://www.youtube.com/watch?v=W25NnjvrZWc'
+    }, {
+      title: 'Sairento VR.',
+      subtitle: 'Intense Shooter. online  Co-op , singleplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Voer drievoudige sprongen, muurrennen, stroomglijbanen en vertraag de tijd terwijl je wegschiet naar een vijand. Bewapen jezelf met katana's, vuurwapens, bogen, werpglaasjes en legendarische relikwieën. Blijf upgraden terwijl je speelt.`,
+      iframe: 'https://www.youtube.com/watch?v=t3jXqjhWefE'
+    }, {
+      title: 'Run Of Mydan.',
+      subtitle: 'Flying Shooter. Singleplayer, campagne, multyplayer',
+      multiplayer: true,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Run Of Mydan is een singleplayer en multiplayer vliegende VR-shooter met verfrissende unieke besturing. Je kunt een reis vol actie ondernemen, vermengd met introspectie. Of online tegen andere spelers vechten met een geweldig vliegbewegingssysteem.`,
+      iframe: 'https://www.youtube.com/watch?v=lE5zC3x_SUQ'
+    }, {
+      title: 'Loco Dojo.',
+      subtitle: 'Skill testing. Singleplayer, multyplayer',
+      multiplayer: true,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Betreed de grillige houten wereld van Loco Dojo en stap naar Grand Sensei's ‘Table of Trials’ om je gekste vaardigheden te testen tegen (maximaal) drie vrienden of vreemden in deze hilarisch competitieve cross-platform multiplayer sociale VR-partygame`,
+      iframe: 'https://www.youtube.com/watch?v=7n58se1FTeE'
+    }, {
+      title: 'Cowbots and Aliens.',
+      subtitle: 'Funny Shooter. Singleplayer, multyplayer',
+      multiplayer: true,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Cowbot Of Alien? Een furieuze PvP Multiplayer-vechtpartij in VR in het Saloon-iverse van Cowbots and Aliens. Vrijbewegend schietspel, meerdere competitieve modes en een aangepast fysiek fysica-systeem waarmee je ALLES kunt gebruiken als wapen. Zorgt voor een enorm bevredigende VR-belevenis!`,
+      iframe: 'https://www.youtube.com/watch?v=6My2ZhoEcpc'
+    }, {
+      title: 'Pistol Whip.',
+      subtitle: 'Bashing true Shooter. Singleplayer',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      Pistol Whip is een niet te stoppen actie-ritme FPS. Reis door een filmische hel, aangedreven door een baanbrekende soundtrack om de ultieme legende van actieheld te worden`,
+      iframe: 'https://www.youtube.com/watch?v=KUwzpaReU5g'
+    }, {
+      title: 'Ninja Legends.',
+      subtitle: 'Swords and ninjas',
+      multiplayer: false,
+      singleplayer: true,
+      pvp: false,
+      description: `
+      De deugd van het lange zwaard kan tien man verslaan. Je moet deze deugd bereiken in Ninja Legends en de gestolen schaduwrollen terugvinden. Vijanden zullen coöperatieve groepsaanvallen gebruiken om je te omringen en proberen te overmeesteren. Beheers alle wapens en tactieken ...`,
+      iframe: 'https://www.youtube.com/watch?v=UqXJnhyE7fQ'
   ], 'vr escape': [
   ], 'vr laser': [
   ]
